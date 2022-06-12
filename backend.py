e9a2b62e62bfd09f82390c2eef6e8f37d1548da6
tritims <tritims@gmail.com>
2022-06-12 13:08:54 +0530

    added keyword extraction

--- backend.py
+++ backend.py
@@ -8,32 +8,32 @@ app = Flask(__name__)
 with open('centroid.pickle', 'rb') as handle:
     centroids = pickle.load(handle)
 
-# from sentence_transformers import SentenceTransformer
-# model = SentenceTransformer('flax-sentence-embeddings/stackoverflow_mpnet-base')
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer('flax-sentence-embeddings/stackoverflow_mpnet-base')
 
-# from keybert import KeyBERT
-# kw_model = KeyBERT(model='flax-sentence-embeddings/stackoverflow_mpnet-base')
+from keybert import KeyBERT
+kw_model = KeyBERT(model='flax-sentence-embeddings/stackoverflow_mpnet-base')
 
-# def getKeywords(text):
-#     kws = kw_model.extract_keywords(text, 
-#                       keyphrase_ngram_range=(2,3), 
-#                       stop_words='english', 
-#                       use_mmr=True, diversity=0.7)
-#     temp = ''
-#     for w, j in kws:
-#         if(j>0.5): # custom threashold
-#             temp = temp + ' ' + w
-#     return temp
+def getKeywords(text):
+    kws = kw_model.extract_keywords(text, 
+                      keyphrase_ngram_range=(2,3), 
+                      stop_words='english', 
+                      use_mmr=True, diversity=0.7)
+    temp = ''
+    for w, j in kws:
+        if(j>0.5): # custom threashold
+            temp = temp + ' ' + w
+    return temp
 
-# def getPattern(text):
-#     pattern = ''
-#     max_score = 0
-#     for centroid in centroids:
-#         t = cosine_similarity([centroids[centroid]], [model.encode(text)])
-#         if t> max_score:
-#             max_score = t
-#             pattern = centroid
-#     return pattern
+def getPattern(text):
+    pattern = ''
+    max_score = 0
+    for centroid in centroids:
+        t = cosine_similarity([centroids[centroid]], [model.encode(text)])
+        if t> max_score:
+            max_score = t
+            pattern = centroid
+    return pattern
 
 # text -> clean -> title + body -> keywords of title & body -> get pattern
 @app.route('/', methods=['POST'])
@@ -41,6 +41,9 @@ def index():
     request_data = request.get_json()
     title = request_data['title']
     body = cp.clean_body(request_data['body']) # body in HTML format
+    title_kws = getKeywords(title)
+    body_kws = getKeywords(body)
+    print(getPattern(title_kws + ' ' + body_kws))
     # print(requst_data['toast'])
     # requst_data['toast'] = " ".join(requst_data['toast'].split())
 
