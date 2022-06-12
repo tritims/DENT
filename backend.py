fcaaa860bf9351bd01625d21eea399bff806fa4d
tritims <tritims@gmail.com>
2022-06-12 10:59:12 +0530

    testing

--- backend.py
+++ backend.py
@@ -39,8 +39,9 @@ def getPattern(text):
 @app.route('/', methods=['POST'])
 def index():
     requst_data = request.get_json()
-    print(requst_data['toast'])
+    # print(requst_data['toast'])
     requst_data = cp.apply_cleaning(requst_data['toast'])
+    print(requst_data)
     return '200'
     
 
