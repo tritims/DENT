2ae367586712793fdf0bd2195b9309ddde89dcc0
tritims <tritims@gmail.com>
2022-06-20 17:08:28 +0530

    majr updates

--- backend.py
+++ backend.py
@@ -4,46 +4,55 @@ import pickle
 from sklearn.metrics.pairwise import cosine_similarity
 import cleanpost as cp
 from flask_cors import CORS, cross_origin
+import numpy as np
 
 app = Flask(__name__)
 CORS(app)
+MODE = 'PROD'
 
-with open('centroid.pickle', 'rb') as handle:
-    centroids = pickle.load(handle)
+if MODE != 'DEV':
+    with open('centroid.pickle', 'rb') as handle:
+        centroids = pickle.load(handle)
 
-from sentence_transformers import SentenceTransformer
-model = SentenceTransformer('flax-sentence-embeddings/stackoverflow_mpnet-base')
+    from sentence_transformers import SentenceTransformer
+    model = SentenceTransformer('flax-sentence-embeddings/stackoverflow_mpnet-base')
 
-from keybert import KeyBERT
-kw_model = KeyBERT(model='flax-sentence-embeddings/stackoverflow_mpnet-base')
+    from keybert import KeyBERT
+    kw_model = KeyBERT(model='flax-sentence-embeddings/stackoverflow_mpnet-base')
 
 def getKeywords(text):
-    kws = kw_model.extract_keywords(text, 
-                      keyphrase_ngram_range=(2,3), 
-                      stop_words='english', 
-                      use_mmr=True, diversity=0.7)
-    temp = ''
-    for w, j in kws:
-        if(j>0.5): # custom threashold
-            temp = temp + ' ' + w
-    return temp
+    if MODE != 'DEV':
+        kws = kw_model.extract_keywords(text, 
+                        keyphrase_ngram_range=(2,3), 
+                        stop_words='english', 
+                        use_mmr=True, diversity=0.7)
+        temp = ''
+        for w, j in kws:
+            if(j>0.5): # custom threashold
+                temp = temp + ' ' + w
+        return temp
+    else:
+        return ''
 
 def getPattern(text):
-    pattern = ''
-    max_score = 0
-    for centroid in centroids:
-        t = cosine_similarity([centroids[centroid]], [model.encode(text)])
-        if t> max_score:
-            max_score = t
-            pattern = centroid
-    return pattern, max_score
+    if MODE != 'DEV':
+        pattern = ''
+        max_score = 0
+        for centroid in centroids:
+            t = cosine_similarity([centroids[centroid]], [model.encode(text)])
+            if t> max_score:
+                max_score = t
+                pattern = centroid
+        return pattern, max_score
+    else:
+        return 'tensor operations', np.array([0.56])
 
 # text -> clean -> title + body -> keywords of title & body -> get pattern
 @app.route('/', methods=['POST'])
 def index():
-    print("inside post...")
+    # print("inside post...")
     request_data = request.get_json()
-    print(request_data)
+    # print(request_data)
     title = request_data['title']
     body = cp.clean_body(request_data['body']) # body in HTML format
     title_kws = getKeywords(title)
--- content.js
+++ content.js
@@ -1,3 +1,9 @@
+// Global Variables
+var CUTOFF = 0.5
+var QUESTION_BODY_CLASSNAME =  'post-layout'
+var TAGS_CLASSNAME = 'post-tag js-gps-track'
+var BASE_URL = 'https://rishalab.github.io/dl_energy_patterns/'
+
 var body = JSON.stringify(
     document
     .getElementsByClassName('s-prose js-post-body')[0]
@@ -7,7 +13,7 @@ var body = JSON.stringify(
 var title = document.getElementById('question-header').getElementsByTagName('h1')[0].innerText
 var DLTags = ['pytorch', 'tensorflow', 'theano', 'caffe', 'keras']
 
-var requestURL = "http://10.21.29.157:3000/"
+var requestURL = "http://localhost:3000/"
 var requestBody = {
     "title": title, 
     "body": body
@@ -15,7 +21,7 @@ var requestBody = {
 
 // Function to verify tags by deep learning posts. 
 function checkIfDL() {
-    tags = document.getElementsByClassName('post-tag js-gps-track')
+    tags = document.getElementsByClassName(TAGS_CLASSNAME)
     for(let tag of tags){
         if(DLTags.includes(tag.innerText)){
             return true
@@ -24,16 +30,54 @@ function checkIfDL() {
     return false
 }
 
+function getPatternURLOffset(tagname){
+    switch(tagname){
+        case 'checkpoint': return 'checkpoint.html';
+        case 'pruning': return 'pruning.html';
+        case 'quantization': return 'quantization.html'; 
+        case 'distillation': return 'distillation.html';
+        case 'efficient read write': return 'read-write.html';
+        case 'memory leaks': return 'leaks.html'; 
+        case 'pretrained networks': return 'pretrained.html';
+        case 'tensor operations': return 'tensor-operations.html';
+        default: return '';
+    }
+}
+
+function generateTag(tagname){
+    e = document.createElement('div')
+    a = document.createElement('a')
+    e.className = 'energytag'
+    e.innerText = tagname
+    a.target = '_blank'
+    a.href = BASE_URL + getPatternURLOffset(tagname)
+    a.appendChild(e)
+    return a
+}
+
+function prependTagToQuestion(e){
+    document.getElementsByClassName(QUESTION_BODY_CLASSNAME)[0].prepend(e)
+}
+
+function tagQuestion(res){
+    if (res.pattern && res.score){
+        if(res.score > CUTOFF){
+            tagElement = generateTag(res.pattern)
+            prependTagToQuestion(tagElement)
+        }
+    }
+}
+
 if(checkIfDL()){
     var xhr = new XMLHttpRequest();  
     xhr.open("POST", requestURL);  
     xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
     xhr.setRequestHeader('Content-type', 'application/json');
     xhr.onreadystatechange = function() { 
-      // If the request completed, close the extension popup
       if (xhr.readyState == 4)
         if (xhr.status == 200){
-            console.log(JSON.parse(xhr.responseText))
+            res = JSON.parse(xhr.responseText)
+            tagQuestion(res)
             // callback(JSON.parse(xhr.responseText))
         }
     };
